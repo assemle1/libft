4773211cb1b10b54ee1fccb63a9469872d785ed8
Assem Talha <astalha@e1r3p15.1337.ma>
2022-10-25 05:54:34 +0100

    first commit

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,32 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: astalha < astalha@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/24 18:26:06 by astalha           #+#    #+#             */
-/*   Updated: 2022/10/25 05:48:53 by astalha          ###   ########.fr       */
+/*   Created: 2022/10/24 22:41:11 by astalha           #+#    #+#             */
+/*   Updated: 2022/10/25 04:26:15 by astalha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int nb, int fd)
+t_list  *ft_lstnew(void *content)
 {
-	long	n;
+    t_list *new;
 
-	n = nb;
-	if (n < 0)
-	{
-		n = -n;
-		ft_putchar_fd ('-', fd);
-	}
-	if (n < 10)
-		ft_putchar_fd (n + 48, fd);
-	else
-	{
-		ft_putnbr_fd (n / 10, fd);
-		ft_putnbr_fd (n % 10, fd);
-	}
+    new = (t_list *) malloc(sizeof(new));
+    if(!new)
+        return (NULL);
+    new->content = content;
+    new->next    = NULL;
+    return (new);
 }
